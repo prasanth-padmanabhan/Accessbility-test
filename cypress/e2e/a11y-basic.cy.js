354d25bd48fe84913eccffdf140f4ebcb4db79cb
prasanth <vvprasanth88@gmail.com>
2025-11-28 11:04:02 +0530

    added server.js and html files for app

--- cypress/e2e/a11y-basic.cy.js
+++ cypress/e2e/a11y-basic.cy.js
@@ -1,10 +1,6 @@
 // cypress/e2e/a11y-basic.cy.js
-describe('Todo app basic accessibility', () => {
-it('has no obvious a11y violations on load', () => {
-cy.visit('http://localhost:3000/todos');
-// Inject axe-core into the page
-cy.injectAxe();
-// Run accessibility checks on the whole page
-cy.checkA11y();
-});
+describe('accessibility testing', () => {
+    beforeEach(()=>{
+        cy.visit('/')
+    })
 });
