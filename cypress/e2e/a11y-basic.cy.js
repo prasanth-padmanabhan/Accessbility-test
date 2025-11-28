28f566fba9b50f18f3e6a6b59e9eb54a49ee3dfa
prasanth <vvprasanth88@gmail.com>
2025-11-28 12:02:48 +0530

    added axe test case with fix

--- cypress/e2e/a11y-basic.cy.js
+++ cypress/e2e/a11y-basic.cy.js
@@ -1,6 +1,12 @@
 // cypress/e2e/a11y-basic.cy.js
 describe('accessibility testing', () => {
     beforeEach(()=>{
-        cy.visit('/')
+        cy.visit('/');
+        cy.injectAxe();
+    })
+    it('Should detect accessibility violations', () => {
+        // check accessibility violations
+        cy.checkA11y();
+
     })
 });
